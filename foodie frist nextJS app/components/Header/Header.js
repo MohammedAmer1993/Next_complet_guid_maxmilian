47e2ee0374c4b75e5fb6ed477e32dcaa28187762
Mohammed Amer <loard.amer.2010@gmail.com>
2025-05-11 15:50:56 +0300

    building our header for foodie app

---
+++ foodie frist nextJS app/components/Header/Header.js
@@ -0,0 +1,29 @@
+import Link from "next/link";
+import Image from "next/image";
+import HeaderBackground from "./HeaderBackground";
+import logo from "@/assets/logo.png";
+import classes from "./Header.module.css";
+export default function MainHeader() {
+  return (
+    <>
+      <HeaderBackground />
+      <header className={classes.header}>
+        <Link href="/" className={classes.logo}>
+          <Image src={logo} alt="table with food covering it" priority />
+          NextLevel Foodie
+        </Link>
+
+        <nav className={classes.nav}>
+          <ul>
+            <li>
+              <Link href="/meals">Browse Meals</Link>
+            </li>
+            <li>
+              <Link href="/community">Foodies Community</Link>
+            </li>
+          </ul>
+        </nav>
+      </header>
+    </>
+  );
+}
