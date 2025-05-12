ea05230605d63ea5f72fa87b446855072a758122
Mohammed Amer <loard.amer.2010@gmail.com>
2025-05-12 16:45:56 +0300

    added meals components

--- foodie frist nextJS app/app/meals/page.js
+++ foodie frist nextJS app/app/meals/page.js
@@ -1,7 +1,25 @@
+import Link from "next/link";
+import MealsGrid from "@/components/meals/MealsGrid";
+import classes from "./page.module.css";
+
 export default function MealsPage() {
   return (
-    <section>
-      <h2>this is meals page</h2>
-    </section>
+    <>
+      <header className={classes.header}>
+        <h1>
+          Delisious meals created by you
+          <span className={classes.highlight}> by you</span>.
+        </h1>
+        <p>
+          Choose your favourite recipe and cook it yourself. It is fun and easy
+        </p>
+        <p className={classes.cta}>
+          <Link href="/meals/share">Share your favourite recipe</Link>
+        </p>
+      </header>
+      <main className={classes.main}>
+        <MealsGrid meals={[]} />
+      </main>
+    </>
   );
 }
